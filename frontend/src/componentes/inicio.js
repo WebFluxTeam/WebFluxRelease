ce6919721c7f0818739e9dd4e6f7b0ccf93da0c3
ErvinRivas <ervin.rivas@correounivalle.edu.co>
2022-10-29 01:18:46 -0500

    mejoras en proceso...

--- frontend/src/componentes/inicio.js
+++ frontend/src/componentes/inicio.js
@@ -2,10 +2,11 @@ import React, {useState} from "react";
 import '../styles/styleInicio.css'
 import Cliente from "./cliente";
 import Admin from "./administrador";
+//import Formulario from "../login/signup"
 
 const Inicio = () => {
     
-    let init =  <div className="blockAdmin">
+    let init =  <div className="blockAdmin" id="ppal">
                     <label className="labelAdmin" ><small>INIT VIEW</small></label>
                     <button onClick={vistaCliente} className="btn"> CLIENTE </button>
                     <button onClick={vistaAdmin} className="btn"> ADMINISTRADOR </button>
@@ -18,7 +19,12 @@ const Inicio = () => {
     }
     function vistaAdmin(){
         setEstado(estado = <Admin/>)  
-    }    
+    }
+
+    /*function sign (){
+        setEstado(estado = <Formulario />)
+        <!--a><button onClick={sign} className="bodyI"> Signup</button></a-->
+    }*/    
 
     return(
         <div>
